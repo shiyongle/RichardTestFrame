ea007dc967d7b7aa787b85f9e90e908d714adf40
richered <richered@yeah.net>
2019-11-18 11:10:57 +0800

    add main目录工程

---
+++ src/main/java/mock/TestMock.java
@@ -0,0 +1,15 @@
+package mock;
+
+public class TestMock {
+    public Integer divid(Integer a, Integer b) {
+        if (b == 0) {
+            return null;
+        } else if (a > 100) {
+            return null;
+        } else if (a < b) {
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
+}
