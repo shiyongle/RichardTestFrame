461b6d3916989dc01f1033faa6975e52e13a3294
richered <richered@yeah.net>
2020-05-28 14:58:44 +0800

    modify

--- src/main/java/com/basics/Employee.java
+++ src/main/java/com/basics/Employee.java
@@ -8,14 +8,22 @@ import java.time.LocalDate;
  * Description:
  */
 public class Employee {
+    private static int nextId = 1;
     private String name;
     private double salary;
     private LocalDate hireDay;
+    private int id;
 
     public Employee(String n, double s, int year, int month, int day) {
         name = n;
         salary = s;
         hireDay = LocalDate.of(year, month, day);
+    }
+
+    public Employee(String n, double s){
+        name = n;
+        salary = s;
+        id = 0;
     }
 
     public String getName() {
@@ -34,4 +42,19 @@ public class Employee {
         double raise = salary * byPercent / 100;
         salary += raise;
     }
+
+    public void setId(){
+        id = nextId;
+        nextId++;
+    }
+
+    public static int getNextId(){
+        return nextId;
+    }
+
+    public static void main(String[] args) {
+        Employee e =new Employee("Harry", 50000);
+        System.out.println(e.getName() + "" + e.getSalary());
+    }
+
 }
