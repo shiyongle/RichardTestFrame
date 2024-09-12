7e7c5eefc7a5896a6ef9da678f7563059348d3c1
shiyl <richered@yeah.net>
2024-09-12 17:41:17 +0800

    modify pom

---
+++ src/main/java/com/beanshell/Cities.java
@@ -0,0 +1,9 @@
+package com.beanshell;
+
+public class Cities {
+    public static final String[] cities = {
+            "110101", "110102", "110105", // 北京市
+            "310101", "310102", "310104", // 上海市
+            // 更多地区码...
+    };
+}
